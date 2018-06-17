6f28b2c3332f2f96d88c3d28d42759316128f775
kuba <jakub.kaminski@psi.ch>
2018-06-17 18:27:34 +0200

    working version of show/hide

--- assets/js/WheelbuilderFrontRearBuildSelection.js
+++ assets/js/WheelbuilderFrontRearBuildSelection.js
@@ -2,7 +2,7 @@ import WheelbuilderConfig from './WheelbuilderConfig.js';
 
 export default class WheelbuilderFrontRearBuildSelection {
     // Depending if customer chosen Wheelset/Front/Rear Wheel hide or show relevant options
-    constructor(option_aliases, all_other_options_on_page, stage_two_options_on_page) {
+    constructor(option_aliases, all_other_options_on_page) {
         this.wb_config = new WheelbuilderConfig();
 
         this.front_wheel_options = this.wb_config.front_wheel_options;
@@ -17,7 +17,6 @@ export default class WheelbuilderFrontRearBuildSelection {
         this.all_other_options_on_page = all_other_options_on_page;
         this.is_front_wheel_hidden = false;
         this.is_rear_wheel_hidden = false;
-        this.stage_two_options_on_page = stage_two_options_on_page
     }
     init() {
         if (this.all_other_options_on_page.hasOwnProperty(this.wb_config.build_type_option_name)) {
@@ -27,7 +26,6 @@ export default class WheelbuilderFrontRearBuildSelection {
 
     }
     get_wheel_build_type() {
-        console.log("Getting");
         const $option_object = this.all_other_options_on_page[this.wb_config.build_type_option_name];
         const $checked_rectangle = $option_object.find('.form-rectangle:checked');
         const $label = $checked_rectangle.parent('.form-label');
@@ -37,63 +35,43 @@ export default class WheelbuilderFrontRearBuildSelection {
 
     }
 
-    __show_hide_action(action, wheel_choice_object) {
-        // Goes through wheel_choice_object (its either front_wheel_options or rear_wheel_options arrays)
-        // and hides option_object given by this label
-        for (let i = 0; i < wheel_choice_object.length; i++) {
-            let option_name = wheel_choice_object[i];
-            let option_object = this.option_aliases.all_options_on_page_aliased[option_name];
-            if (action === 'show') {
-                option_object.show();
-            } else {
-                option_object.hide();
-            }
-
-        }
-    }
-
-    rear_wheel_action(action, stage_one_finished, stage_two_finished){
-        // if action can be 'hide' or 'show'
-        if (stage_two_finished === true) {
-            this.__show_hide_action(action, this.rear_wheel_options);
-        }
-        else if (stage_one_finished === true) {
-            this.__show_hide_action(action, this.rear_wheel_stage_2_options);
-        }
-    }
-
-    front_wheel_action(action, stage_one_finished, stage_two_finished){
-        // if action can be 'hide' or 'show'
-        if (stage_two_finished === true) {
-            this.__show_hide_action(action, this.front_wheel_options);
-        }
-        else if (stage_one_finished === true) {
-            this.__show_hide_action(action, this.front_wheel_stage_2_options);
-        }
-
-    }
-
-    show_hide_front_rear(stage_one_finished, stage_two_finished){
+    get_front_rear_options_to_hide(stage_one_finished){
         if (stage_one_finished === true) { //hide unhide Front Rear options only when stage one is completed
             let wheel_build_type = this.get_wheel_build_type();
             if (wheel_build_type === 'Front Wheel') {
-                this.rear_wheel_action('hide', stage_one_finished, stage_two_finished);
-                this.front_wheel_action('show', stage_one_finished, stage_two_finished);
+                return this.rear_wheel_options;
             } else if (wheel_build_type === 'Rear Wheel') {
-                this.rear_wheel_action('show', stage_one_finished, stage_two_finished);
-                this.front_wheel_action('hide', stage_one_finished, stage_two_finished);
+                return this.front_wheel_options;
             } else { //Wheelset
-                this.rear_wheel_action('show', stage_one_finished, stage_two_finished);
-                this.front_wheel_action('show', stage_one_finished, stage_two_finished);
+                return [];
             }
         }
     }
+
+
     disable_selections() {
         const $option_object = this.all_other_options_on_page[this.wb_config.build_type_option_name];
         const $options = $option_object.find('.form-rectangle');
         $options.each(function() {
-            console.log('Disablong', $(this));
             $(this).attr('disabled', 'disabled');
         });
+    }
+
+    reset_selection(){
+        const $option_object = this.all_other_options_on_page[this.wb_config.build_type_option_name];
+        const $options = $option_object.find('.form-rectangle');
+        let first_elem = $options.first();
+        $(first_elem).prop('checked', true);
+
+    }
+
+    hide_selections_buttons() {
+        const $option_object = this.all_other_options_on_page[this.wb_config.build_type_option_name];
+        $option_object.hide();
+
+    }
+    show_selections_buttons() {
+        const $option_object = this.all_other_options_on_page[this.wb_config.build_type_option_name];
+        $option_object.show();
     }
 }
